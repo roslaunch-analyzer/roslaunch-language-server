8334ee5273866ed8ef299e979382fe3cda888148
samrat.thapa <samratthapa120@gmail.com>
2024-05-23 17:47:47 +0900

    port change

--- src/roslaunch_language_server/cli.py
+++ src/roslaunch_language_server/cli.py
@@ -6,7 +6,7 @@ cli = typer.Typer()
 
 
 @cli.command()
-def run(port: int = 8083):
+def run(port: int = None):
     from roslaunch_language_server.sever import server
 
     print(f"Starting roslaunch-language-server on port {port}")
