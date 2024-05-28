2d96d8fa6db8522f0253e630adbd3d925c06ae1e
Y.Hisaki <yhisaki31@gmail.com>
2024-05-28 18:56:28 +0900

    remove unused
    Signed-off-by: Y.Hisaki <yhisaki31@gmail.com>

--- examples/example_roslaunch_analyzer_v2.py
+++
@@ -1,9 +0,0 @@
-from roslaunch_analyzer_v2.command import command2tree
-
-tree = command2tree(
-    "ros2 launch autoware_launch planning_simulator.launch.xml "
-    "map_path:=$HOME/autoware_map/sample-map-planning/ "
-    "vehicle_model:=sample_vehicle"
-)
-
-tree.build()
